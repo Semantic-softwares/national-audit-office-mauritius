a1c83028776afa24bf4a260fff935c03fb61510b
Alex Obogbare <Onozorgheneho1@>
2026-05-13 10:52:49 +0400

    fix mobile

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'reports/:category', renderMode: RenderMode.Server },
+  { path: 'events/:slug',      renderMode: RenderMode.Server },
+  { path: 'news/:slug',        renderMode: RenderMode.Server },
+  { path: 'staff/:slug',       renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
